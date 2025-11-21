1282c3a540ddb5604070a7f13db8aaf320beb108
insanetim <47120724+insanetim@users.noreply.github.com>
2025-11-21 13:49:23 +0200

    setup tailwind / daisy ui / icons

--- app/app.vue
+++ app/app.vue
@@ -3,5 +3,8 @@
     <h1 class="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <button class="btn btn-secondary">
+      View Map <Icon name="tabler:map" />
+    </button>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
-  css: ["./app/assets/css/main.css"],
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  css: ["~/assets/css/main.css"],
   eslint: {
     config: {
       standalone: false,
