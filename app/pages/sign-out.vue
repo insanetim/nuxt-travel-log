e2406985c050d4b8753964fbdb2ec113fa71e734
Timur Medvedev <47120724+insanetim@users.noreply.github.com>
2025-11-22 23:41:42 +0200

    add auth state / sign out

---
+++ app/pages/sign-out.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.signOut();
+});
+</script>
+
+<template>
+  <div class="card bg-base-300 container mt-4 mx-auto min-h-72 text-center flex flex-col justify-center items-center gap-4">
+    <span class="loading loading-spinner loading-xl" />
+  </div>
+</template>
