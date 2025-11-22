746b2eb62fb7c99de9ca2a11c2bd5de28a7dc26e
insanetim <47120724+insanetim@users.noreply.github.com>
2025-11-22 16:05:02 +0200

    setup github oauth

---
+++ app/components/auth-button.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { createAuthClient } from "better-auth/client";
+
+const authClient = createAuthClient();
+const loading = ref(false);
+
+async function signIn() {
+  loading.value = true;
+  await authClient.signIn.social({
+    provider: "github",
+    callbackURL: "/dashboard",
+  });
+  loading.value = false;
+}
+</script>
+
+<template>
+  <button
+    class="btn btn-accent"
+    :disabled="loading"
+    @click="signIn"
+  >
+    Sign In With GitHub
+    <span v-if="loading" class="loading loading-spinner loading-md" />
+    <Icon
+      v-else
+      name="tabler:brand-github"
+      size="24"
+    />
+  </button>
+</template>
