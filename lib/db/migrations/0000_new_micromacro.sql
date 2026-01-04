110dc3f0de80f405166a08f8681e60bdac22edee
Timur Medvedev <47120724+insanetim@users.noreply.github.com>
2026-01-04 13:19:41 +0200

    make location slug unique (#15)
    * add slug unique error
    
    * add unique name check
    
    * Update lint.yaml
    
    * update

--- lib/db/migrations/0000_new_micromacro.sql
+++ lib/db/migrations/0000_new_micromacro.sql
@@ -58,13 +58,14 @@ CREATE TABLE `location` (
 	`description` text,
 	`lat` real NOT NULL,
 	`long` real NOT NULL,
-	`user_id` integer NOT NULL,
+	`user_id` text NOT NULL,
 	`created_at` integer NOT NULL,
 	`updated_at` integer NOT NULL,
 	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `location_slug_unique` ON `location` (`slug`);--> statement-breakpoint
+CREATE UNIQUE INDEX `location_name_userId_unique` ON `location` (`name`,`user_id`);--> statement-breakpoint
 CREATE TABLE `locationLog` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`name` text NOT NULL,
@@ -74,7 +75,7 @@ CREATE TABLE `locationLog` (
 	`lat` real NOT NULL,
 	`long` real NOT NULL,
 	`location_id` integer NOT NULL,
-	`user_id` integer NOT NULL,
+	`user_id` text NOT NULL,
 	`created_at` integer NOT NULL,
 	`updated_at` integer NOT NULL,
 	FOREIGN KEY (`location_id`) REFERENCES `location`(`id`) ON UPDATE no action ON DELETE no action,
@@ -85,7 +86,7 @@ CREATE TABLE `locationLogImage` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`key` text NOT NULL,
 	`location_log_id` integer NOT NULL,
-	`user_id` integer NOT NULL,
+	`user_id` text NOT NULL,
 	`created_at` integer NOT NULL,
 	`updated_at` integer NOT NULL,
 	FOREIGN KEY (`location_log_id`) REFERENCES `locationLog`(`id`) ON UPDATE no action ON DELETE no action,
