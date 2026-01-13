e789e4d4aac02b9c1bbf8298f7655d2a52685ac0
Timur Medvedev <47120724+insanetim@users.noreply.github.com>
2026-01-13 15:19:02 +0200

    show list of lications in sidebar (#18)
    * add sidebar items and skeleton
    
    * add location store

--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -1,8 +1,14 @@
 <script lang="ts" setup>
 const isSidebarOpen = ref(true);
+const route = useRoute();
+const sidebarStore = useSidebarStore();
+const locationStore = useLocationStore();
 
 onMounted(() => {
   isSidebarOpen.value = localStorage.getItem("isSidebarOpen") === "true";
+  if (route.path !== "/dashboard") {
+    locationStore.refresh();
+  }
 });
 
 function toggleSidebar() {
@@ -43,6 +49,20 @@ function toggleSidebar() {
           icon="tabler:circle-plus-filled"
           href="/dashboard/add"
         />
+        <div v-if="sidebarStore.loading || sidebarStore.sidebarItems.length" class="divider" />
+        <div v-if="sidebarStore.loading" class="px-4">
+          <div class="skeleton h-4 w-full" />
+        </div>
+        <div v-if="!sidebarStore.loading && sidebarStore.sidebarItems.length" class="flex flex-col">
+          <SidebarButton
+            v-for="item in sidebarStore.sidebarItems"
+            :key="item.id"
+            :label="item.label"
+            :icon="item.icon"
+            :href="item.href"
+            :show-label="isSidebarOpen"
+          />
+        </div>
         <div class="divider" />
         <SidebarButton
           :show-label="isSidebarOpen"
