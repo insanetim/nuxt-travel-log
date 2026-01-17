97131f38f40e81d051eaa5b33b0ba6c82012e0a2
Timur Medvedev <47120724+insanetim@users.noreply.github.com>
2026-01-17 13:21:34 +0200

    setup map (#19)
    * add maplibre module
    
    * Update map.client.vue
    
    * add map dark style
    
    * Update package-lock.json

---
+++ app/components/app/map.client.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { CENTER_DNIPRO } from "~~/lib/constants";
+
+const colorMode = useColorMode();
+const style = computed(() => colorMode.value === "dark" ? "styles/dark.json" : "https://tiles.openfreemap.org/styles/liberty");
+const zoom = 3;
+</script>
+
+<template>
+  <MglMap
+    :map-style="style"
+    :center="CENTER_DNIPRO"
+    :zoom="zoom"
+  >
+    <MglNavigationControl />
+  </MglMap>
+</template>
---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+import type { LngLatLike } from "maplibre-gl";
+
+export const CENTER_DNIPRO: LngLatLike = [34.983334, 48.450001];
