6f208ac7cc0edd2a81e872627946cf6dd72ba1f2
insanetim <47120724+insanetim@users.noreply.github.com>
2026-03-04 15:44:38 +0200

    make reusable location card component

---
+++ app/components/app/location-card.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import type { MapPoint } from "~~/lib/types";
+
+defineProps<{
+  mapPoint: MapPoint;
+}>();
+
+const mapStore = useMapStore();
+</script>
+
+<template>
+  <NuxtLink
+    :to="mapPoint.to"
+    class="card bg-base-300 h-40 w-72 mb-2 border-2 shrink-0 hover:cursor-pointer"
+    :class="{
+      'border-accent': isPointSelected(mapPoint, mapStore.selectedPoint),
+      'border-transparent': !isPointSelected(mapPoint, mapStore.selectedPoint),
+    }"
+    @mouseenter="mapStore.selectedPoint = mapPoint"
+    @mouseleave="mapStore.selectedPoint = null"
+  >
+    <div class="card-body">
+      <h3 class="text-xl">
+        {{ mapPoint.name }}
+      </h3>
+      <p>{{ mapPoint.description }}</p>
+    </div>
+  </NuxtLink>
+</template>
--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
+import LocationCard from "~/components/app/location-card.vue";
+
 const locationStore = useLocationStore();
-const mapStore = useMapStore();
 const { locations, locationsStatus: status } = storeToRefs(locationStore);
 
 onMounted(() => {
@@ -17,25 +18,11 @@ onMounted(() => {
       <span class="loading loading-spinner loading-xl" />
     </div>
     <div v-else-if="locations && locations.length" class="flex mt-4 gap-2 overflow-auto">
-      <NuxtLink
+      <LocationCard
         v-for="location in locations"
         :key="location.id"
-        :to="{ name: 'dashboard-location-slug', params: { slug: location.slug } }"
-        class="card bg-base-300 h-40 w-72 mb-2 border-2 shrink-0 hover:cursor-pointer"
-        :class="{
-          'border-accent': isPointSelected(location, mapStore.selectedPoint),
-          'border-transparent': !isPointSelected(location, mapStore.selectedPoint),
-        }"
-        @mouseenter="mapStore.selectedPoint = createMapPointFromLocation(location)"
-        @mouseleave="mapStore.selectedPoint = null"
-      >
-        <div class="card-body">
-          <h3 class="text-xl">
-            {{ location.name }}
-          </h3>
-          <p>{{ location.description }}</p>
-        </div>
-      </NuxtLink>
+        :map-point="createMapPointFromLocation(location)"
+      />
     </div>
     <div v-else class="flex flex-col gap-2 mt-4">
       <p>Add a location to get started</p>
