d56c40245f12a52d8312ceccd3f174e5d602ba45
Timur Medvedev <47120724+insanetim@users.noreply.github.com>
2026-03-24 15:20:36 +0200

    s3 storage setup (#31)
    * add docker-compose
    
    * change lint.yaml

--- lib/env.ts
+++ lib/env.ts
@@ -10,6 +10,11 @@ const EnvSchema = z.object({
   BETTER_AUTH_URL: z.string(),
   AUTH_GITHUB_CLIENT_ID: z.string(),
   AUTH_GITHUB_CLIENT_SECRET: z.string(),
+  S3_ENDPOINT: z.string(),
+  S3_ACCESS_KEY: z.string(),
+  S3_SECRET_KEY: z.string(),
+  S3_REGION: z.string(),
+  S3_BUCKET: z.string(),
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
